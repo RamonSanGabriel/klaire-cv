e654e6cfbe4b0e5c1308db7c7daa78d63491ba37
RamonSanGabriel <jeshero11@gmail.com>
2025-04-01 17:12:42 +0800

    Update training.js

--- klaire-CV/src/data/training.js
+++ klaire-CV/src/data/training.js
@@ -39,4 +39,64 @@ export const training = [
     id: 10,
     training: 'Hydrafacial',
   },
+  {
+    id: 11,
+    training: 'Lutronic Action II Er:YAG Fractional Laser',
+  },
+  {
+    id: 12,
+    training: 'Lutronic Spectra XT Q-Switched Nd:yag laser',
+  },
+  {
+    id: 13,
+    training: 'Zeltiq Coolsculpting',
+  },
+  {
+    id: 14,
+    training: 'Velashape III',
+  },
+  {
+    id: 15,
+    training: 'Regen Lab Centrifuge (used for PRP)',
+  },
+  {
+    id: 16,
+    training: 'Eclipse Derma Pen',
+  },
+  {
+    id: 17,
+    training: 'Eclipse Derma Pen',
+  },
+  {
+    id: 18,
+    training: 'Duo Pro Nd:YAG + Alexandrite Laser',
+  },
+  {
+    id: 19,
+    training: 'QS Ultralight Q-switched laser',
+  },
+  {
+    id: 20,
+    training: 'Dana CO2 Fractional Laser',
+  },
+  {
+    id: 21,
+    training: 'Silk Peel Dermal Infusion',
+  },
+  {
+    id: 22,
+    training: 'Thermage Radiofrequency',
+  },
+  {
+    id: 23,
+    training: 'Fraxel Dual',
+  },
+  {
+    id: 24,
+    training: 'Fotona SP Dynamis Nd:YAG + Er:YAG laser',
+  },
+  {
+    id: 25,
+    training: 'Fotona QX Max Q-switched laser',
+  },
 ];
