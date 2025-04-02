a9a0083c59714ef56723f5e97783ff80646e776a
RamonSanGabriel <jeshero11@gmail.com>
2025-04-02 22:36:34 +0800

    Update main.jsx

--- klaire-CV/src/main.jsx
+++ klaire-CV/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter base href="%PUBLIC_URL%/">
       <App />
     </BrowserRouter>
   </StrictMode>
