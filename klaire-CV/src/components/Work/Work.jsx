78f60db7c3e47c90639250c79bb519609aa3beda
RamonSanGabriel <jeshero11@gmail.com>
2025-04-04 14:28:15 +0800

    work7.js

--- klaire-CV/src/components/Work/Work.jsx
+++ klaire-CV/src/components/Work/Work.jsx
@@ -1,5 +1,13 @@
 import css from './Work.module.css';
-import { work, work2, work3, work4 } from '../../data/work';
+import {
+  work,
+  work2,
+  work3,
+  work4,
+  work5,
+  work6,
+  work7,
+} from '../../data/work';
 
 const Work = () => {
   return (
@@ -8,22 +16,8 @@ const Work = () => {
         <ul>
           <h1 className={css.header}></h1>
           {work.map(
-            ({
-              id,
-              position,
-              company,
-              duration,
-              location,
-              tasks1,
-              tasks2,
-              tasks3,
-              tasks4,
-              tasks5,
-              tasks6,
-              tasks7,
-              tasks8,
-              tasks9,
-              tasks10,
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6,tasks7,tasks8,tasks9,tasks10,
             }) => (
               <li className={css.workList} key={id}>
                 <h3 className={css.position}>{position}</h3>
@@ -50,21 +44,8 @@ const Work = () => {
 
         <ul>
           {work2.map(
-            ({
-              id,
-              position,
-              company,
-              duration,
-              location,
-              tasks1,
-              tasks2,
-              tasks3,
-              tasks4,
-              tasks5,
-              tasks6,
-              tasks7,
-              tasks8,
-              tasks9,
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6,tasks7,tasks8,tasks9,
             }) => (
               <li className={css.workList} key={id}>
                 <h3 className={css.position}>{position}</h3>
@@ -90,18 +71,8 @@ const Work = () => {
 
         <ul>
           {work3.map(
-            ({
-              id,
-              position,
-              company,
-              duration,
-              location,
-              tasks1,
-              tasks2,
-              tasks3,
-              tasks4,
-              tasks5,
-            }) => (
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5}) => (
               <li className={css.workList} key={id}>
                 <h3 className={css.position}>{position}</h3>
                 <p className={css.company}>{company}</p>
@@ -122,18 +93,31 @@ const Work = () => {
 
         <ul>
           {work4.map(
-            ({
-              id,
-              position,
-              company,
-              duration,
-              location,
-              tasks1,
-              tasks2,
-              tasks3,
-              tasks4,
-              tasks5,
-              tasks6,
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6}) => (
+              <li className={css.workList} key={id}>
+                <h3 className={css.position}>{position}</h3>
+                <p className={css.company}>{company}</p>
+                <p className={css.duration}>{duration}</p>
+                <p className={css.location}>{location}</p>
+                <div className={css.workListItemContainer}>
+                  <p className={css.workListItem}>{tasks1}</p>
+                  <p className={css.workListItem}>{tasks2}</p>
+                  <p className={css.workListItem}>{tasks3}</p>
+                  <p className={css.workListItem}>{tasks4}</p>
+                  <p className={css.workListItem}>{tasks5}</p>
+                  <p className={css.workListItem}>{tasks6}</p>
+                </div>
+                <hr />
+              </li>
+            )
+          )}
+        </ul>
+
+        <ul>
+          {work5.map(
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6
             }) => (
               <li className={css.workList} key={id}>
                 <h3 className={css.position}>{position}</h3>
@@ -147,6 +131,58 @@ const Work = () => {
                   <p className={css.workListItem}>{tasks4}</p>
                   <p className={css.workListItem}>{tasks5}</p>
                   <p className={css.workListItem}>{tasks6}</p>
+                </div>
+                <hr />
+              </li>
+            )
+          )}
+        </ul>
+        <ul>
+          {work6.map(
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6,tasks7,tasks8,tasks9,tasks10,tasks11,tasks12,tasks13
+            }) => (
+              <li className={css.workList} key={id}>
+                <h3 className={css.position}>{position}</h3>
+                <p className={css.company}>{company}</p>
+                <p className={css.duration}>{duration}</p>
+                <p className={css.location}>{location}</p>
+                <div className={css.workListItemContainer}>
+                  <p className={css.workListItem}>{tasks1}</p>
+                  <p className={css.workListItem}>{tasks2}</p>
+                  <p className={css.workListItem}>{tasks3}</p>
+                  <p className={css.workListItem}>{tasks4}</p>
+                  <p className={css.workListItem}>{tasks5}</p>
+                  <p className={css.workListItem}>{tasks6}</p>
+                  <p className={css.workListItem}>{tasks7}</p>
+                  <p className={css.workListItem}>{tasks8}</p>
+                  <p className={css.workListItem}>{tasks9}</p>
+                  <p className={css.workListItem}>{tasks10}</p>
+                  <p className={css.workListItem}>{tasks11}</p>
+                  <p className={css.workListItem}>{tasks12}</p>
+                  <p className={css.workListItem}>{tasks13}</p>
+                </div>
+                <hr />
+              </li>
+            )
+          )}
+        </ul>
+        <ul>
+          {work7.map(
+            // prettier-ignore
+            ({id,position,company,duration,location,tasks1,tasks2,tasks3,tasks4,tasks5,tasks6,tasks7,tasks8,tasks9,tasks10,tasks11,tasks12,tasks13
+            }) => (
+              <li className={css.workList} key={id}>
+                <h3 className={css.position}>{position}</h3>
+                <p className={css.company}>{company}</p>
+                <p className={css.duration}>{duration}</p>
+                <p className={css.location}>{location}</p>
+                <div className={css.workListItemContainer}>
+                  <p className={css.workListItem}>{tasks1}</p>
+                  <p className={css.workListItem}>{tasks2}</p>
+                  <p className={css.workListItem}>{tasks3}</p>
+                  <p className={css.workListItem}>{tasks4}</p>
+                  <p className={css.workListItem}>{tasks5}</p>
                 </div>
                 <hr />
               </li>
--- klaire-CV/src/data/work.js
+++ klaire-CV/src/data/work.js
@@ -101,3 +101,81 @@ export const work4 = [
     tasks6: "Made appointments, follow-up on patient, or perform other clerical duties in doctors' office or clinics.",
   },
 ];
+export const work5 = [
+  {
+    id: 5,
+    position: 'Staff Nurse DHA Licensed',
+    company: 'Dr. Taher Hassan Khalil Clinic',
+    duration: 'Nov 2012-May 2014',
+    location: 'Dubai, U.A.E',
+    tasks1:
+      'Assessed and records relevant details from the patient such as vital signs, objective and subjective complaints, health history etc.',
+    tasks2:
+      'Prepared patients for procedures (Ultrasound), examinations, tests, or treatments.',
+    tasks3:
+      'Carried out orders from the doctor such as skin testing for allergic reaction, administer oral and injectable medications recording time, dosage and any adverse effects of medication, wound cleaning and dressing, suture removal etc.',
+    tasks4:
+      'Assisted the doctor in performing minor treatments and assist in laser treatments (Laser Hair Removal using Nd:YAG Laser, Laser treatment of onycomycosis, spider vein treatments, Skin Resurfacing using Er:YAG Laser, Laser Vaginal Rejuvenation etc.). Laser used Fotona.',
+    tasks5: 'Collected blood sample from patients for laboratory analysis.',
+    tasks6:
+      'Gave instructions to patients in taking medication, proper collection of specimens such as urine, semen, stool samples, use a pregnancy and LH surge kit etc.',
+    tasks7:
+      'Worked as part of a healthcare team to assess patient needs, plan and modify care, implement interventions and evaluate nursing intervention outcomes.',
+    tasks8:
+      // prettier-ignore
+      "Sterilized equipment and supplies after each use and ensure treatment rooms is clean to maintain a safe, therapeutic, and hygienic environment; ensures adequate supplies are available and that equipment's are in good working condition.",
+    tasks9:
+      // prettier-ignore
+      "Made appointments, keep records, or perform other clerical duties in doctors' office or clinics.",
+  },
+];
+export const work6 = [
+  {
+    id: 6,
+    position: 'Administrative Assistant cum Sales and Marketing Executive',
+    company: 'Dr. Taher Hassan Khalil Clinic',
+    duration: 'Sep 2011-Nov 2012',
+    location: 'Dubai, U.A.E',
+    tasks1:
+      'Provided customer service to clients. (Answer telephone calls, inquiries, emails and direct them to person in charge if necessary)',
+    tasks2:
+      'Provided administrative support to ensure that operations are maintained in an effective, up to date and accurate manner. (Maintain office files, type correspondence, reports and other documents).',
+    tasks3:
+      'Maintained confidential records and files (customer data base, customer profiles and price list)',
+    tasks4: 'Gathered Market and customer information.',
+    tasks5:
+      'Contacted and followed-up clients to obtain good communication and to collect orders from clients.',
+    tasks6:
+      'Prepared marketing materials needed for exhibition stands, meetings, workshops and conferences.',
+    tasks7:
+      'Scheduled sales meeting and other important meeting with regards to promotion of clinic services/ laser equipment.',
+    tasks8:
+      'Monitored on going and upcoming activities such as Dubai Derma, AEEDC (UAE International Dental Conference & Arab Dental Exhibition), SULTARC (Sharjah University Laser Training and Research Center), and other Laser Conferences in UAE and other countries.',
+    tasks9:
+      'Represented the Light Care International at trade exhibitions, events and demonstrations.',
+    tasks10: 'Maintained stationary supplies and coordinating deliveries.',
+    tasks11:
+      'Oversaw all administrative aspects of seminars including registrations, confirmations, certificates, badges & evaluations, preparation and mailing of materials for training.',
+    tasks12:
+      'Functioned as a Training Coordinator and Organizer for SULTARC (Sharjah University Laser Training and Research Center)',
+    tasks13:
+      'Coordinated with Sharjah University in booking training facilities, requisition of supplies for training and documents pertaining to workshops/trainings.',
+  },
+];
+export const work7 = [
+  {
+    id: 7,
+    position: 'Public Health Nurse',
+    company: 'Makati Health Department',
+    duration: 'May 2009-Jun 2011',
+    location: 'Makati, Philippines',
+    tasks1: 'The promotion and prevention of health thru health education.',
+    tasks2:
+      'Implemented the Philippines department of health programs such as expanded program for immunization, providing family planning counseling, national tuberculosis program (DOTS), prenatal and postnatal care etc.',
+    tasks3:
+      'Provided primary care to patients in the community by assessing clients, giving first aid, carry out doctor’s order such as administering oral and injectable medications to both adult and infant, collecting vaginal or cervical swabs, wound cleaning and referring them when they need tertiary or specialized care to specialty hospitals.',
+    tasks4:
+      'Assisted the OB-sonographer and prepare clients for ultrasound (pelvic, trans-vaginal and rectal)',
+    tasks5: 'Performed and operated ECG Machine.',
+  },
+];
